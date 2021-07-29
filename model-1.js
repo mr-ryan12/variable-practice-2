eca40d01764679bf1ae38b30780363b957bfae90
Ryan McBride <mr.ryan444@gmail.com>
2021-07-28 23:40:50 -0600

    Add string, number, array and object variables

--- model-1.js
+++ model-1.js
@@ -1,8 +1,39 @@
 var str1 = 'This is some string';
 var str2 = 'This is another string';
+var str3 = 'This is another another string';
+
 var num1 = 1;
 var num2 = 2;
+var num3 = 3;
+
 var bool1 = true;
 var bool2 = false;
+var bool3 = true
+
 var arr1 = [1, 2, 3, 4, 5];
 var arr2 = ['This', 'is', 'an', 'array', 'of', 'strings', '!'];
+var arr3 = [true, false, false, true, false, true];
+
+var obj1 = {
+    name: 'Some Guy',
+    age: 32,
+    city: 'Dallas',
+    needsHaircut: true,
+};
+
+var obj2 = {
+    name: 'Lulu',
+    age: 7,
+    breed: 'Lab mix',
+    color: 'Brown',
+    toys: ['squirrels', 'anything the other dogs have', 'doorknobs']
+};
+
+var obj3 = {
+    make: 'Dodge',
+    model: 'Ram',
+    year: 2006,
+    color: 'beige',
+    miles: 142000,
+    engineOn: false,
+};
